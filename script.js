9adc8f774951c5c50b7207f255efadccc6e586c2
Gabriel <gabrieljssouza@gmail.com>
2023-01-23 14:35:52 -0300

    Privadao

--- script.js
+++ script.js
@@ -1,119 +1,14 @@
-const btnAction = document.querySelector('#button-random-color');
-const collorPallete = document.querySelector('#color-palette');
-const input = document.querySelector('#board-size');
-const buttonBoard = document.querySelector('#generate-board');
-const pixelsBoard = document.querySelector('#pixel-board');
+const btnLogin = document.querySelector('#btn-login');
 
-function generateColorAleatory() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let index = 0; index < 6; index += 1) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
+const verifyLogin = () => {
+  const email = document.getElementById('email');
+  const password = document.getElementById('password');
 
-const generateTabel = () => {
-  const tablePixel = document.querySelector('#pixel-board');
-  for (let index = 0; index < 11; index += 1) {
-    const lines = document.createElement('div');
-    lines.className = 'pixels2';
-    for (let index2 = 0; index2 < 11; index2 += 1) {
-      const cells = document.createElement('div');
-      cells.className = 'pixel';
-      lines.appendChild(cells);
-    }
-    tablePixel.appendChild(lines);
-  }
-};
-
-generateTabel();
-const salvegeColor = () => {
-  const getItem = localStorage.getItem('colorPalette');
-  const collorPallete2 = document.querySelector('#color-palette');
-  collorPallete2.innerHTML = getItem;
-};
-
-const paintColor = () => {
-  const colorBox = document.querySelectorAll('.color');
-  for (let index = 0; index < colorBox.length; index += 1) {
-    if (colorBox[index] === colorBox[0] || colorBox[index] === colorBox[4]) {
-      colorBox[0].style.backgroundColor = 'black';
-      colorBox[4].style.backgroundColor = 'white';
-    } else {
-      colorBox[index].style.backgroundColor = generateColorAleatory();
-    }
-  }
-  localStorage.setItem('colorPalette', collorPallete.innerHTML);
-};
-
-btnAction.addEventListener('click', paintColor);
-
-const selectedColor = () => {
-  const colorBox = document.getElementsByClassName('color');
-  for (let index = 0; index < colorBox.length; index += 1) {
-    colorBox[index].addEventListener('click', (event) => {
-      const selected = document.querySelector('.selected');
-      if (selected) {
-        selected.classList.remove('selected');
-      }
-      event.target.classList.add('selected');
-    });
-  }
-};
-
-const saveArtLocal = () => {
-  const pixel = document.querySelectorAll('.pixel');
-  const array = [];
-  for (let index = 0; index < pixel.length; index += 1) {
-    array.push(pixel[index].style.backgroundColor);
-    localStorage.setItem('pixelBoard', JSON.stringify(array));
-  }
-};
-
-const colored = () => {
-  const pixel = document.querySelectorAll('.pixel');
-  for (let index = 0; index < pixel.length; index += 1) {
-    pixel[index].addEventListener('click', () => {
-      const selected = document.querySelector('.selected');
-      pixel[index].style.backgroundColor = selected.style.backgroundColor;
-      saveArtLocal();
-    });
-  }
-};
-
-const getArtLocal = () => {
-  const getPixel = JSON.parse(localStorage.getItem('pixelBoard'));
-  const pixel = document.querySelectorAll('.pixel');
-  for (let index = 0; index < pixel.length; index += 1) {
-    pixel[index].style.backgroundColor = getPixel[index];
-  }
-};
-
-const clear = () => {
-  const clearButton = document.querySelector('#clear-board');
-  const pixel = document.querySelectorAll('.pixel');
-  clearButton.addEventListener('click', () => {
-    for (let index = 0; index < pixel.length; index += 1) {
-      pixel[index].style.backgroundColor = 'white';
-    }
-  });
-};
-
-window.onload = () => {
-  if (localStorage.getItem('colorPalette') === null) {
-    paintColor();
+  if (email.value === 'tryber@test.com' && password.value === '123456') {
+    alert('Olá, Tryber!');
   } else {
-    salvegeColor();
+    alert('Email ou senha inválidos');
   }
-
-  if (localStorage.getItem('pixelBoard') === null) {
-    colored();
-  } else {
-    getArtLocal();
-  }
-
-  selectedColor();
-  colored();
-  clear();
 };
+
+btnLogin.addEventListener('click', verifyLogin);
